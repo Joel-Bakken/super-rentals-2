9349483f076ed9633da24bb900e76b54423a983d
Esvethlynna Pangelinan <esveth@aol.com>
2017-08-07 16:45:48 -0700

    add some code for Find Record

--- app/router.js
+++ app/router.js
@@ -9,6 +9,8 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
+  //Now, when the router receives a request matching /rental/:rental_id (such as from a link within our application that contains the rental's id) it will map the URL to the rental route handler, rental.js, and send a params hash that includes the value of the :rental_id segment in the URL.
+  this.route('rental', {path: '/rental/:rental_id'});
 });
 
 export default Router;
