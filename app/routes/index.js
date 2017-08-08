34cf326faad9890f52c3e11834e9ac345d8df60b
Esvethlynna Pangelinan and Lincoln Nguyen <lincoln@>
2017-08-08 09:17:39 -0700

    finish monday exercise

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,30 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-//we specify this.store to refer to the Firebase data store we've set up for our application. Then the findAll method with the argument rental instructs Ember Data to find all records of the type rental in the store, and return them to our application.
   model() {
-    return this.store.findAll('rental');
-  },
-
-  actions: {
+     return this.store.findAll('rental');
+   },
+   actions: {
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
-      this.transitionTo('index');
-    },
-
-    update(rental, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          rental.set(key,params[key]);
-        }
-      });
-      rental.save();
-      this.transitionTo('index');
-    },
-
-    destroyRental(rental) {
-      rental.destroyRecord();
       this.transitionTo('index');
     }
   }
