0aa751d9450b78c69049e09278a2055b01fdab3c
Esvethlynna Pangelinan and Joel Bakken <joel@>
2017-08-09 09:37:02 -0700

    follow Wednesday exercises

--- app/components/rental-detail.js
+++ app/components/rental-detail.js
@@ -1,13 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isImageShowing: false,
   actions: {
-    imageShow: function() {
-      this.set('isImageShowing', true);
+    delete(rental) {
+      if (confirm('Are you sure you want to delete this rental?')) {
+        this.sendAction('destroyRental', rental);
+      }
     },
-    imageHide: function() {
-     this.set('isImageShowing', false);
-   }
+    destroyReview(review) {
+      this.sendAction('destroyReview', review);
+    }
   }
 });
